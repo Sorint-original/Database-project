f1f7aa6ad37027cd83eae12c44b19e835ca53f63
Adrianosa1 <144953774+Adrianosa1@users.noreply.github.com>
2024-03-26 16:04:53 +0200

    revenue

---
+++ SomerenApp/SomerenUI/RevenueReport.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace SomerenUI
+{
+    partial class RevenueReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // RevenueReport
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1200, 698);
+            Name = "RevenueReport";
+            Text = "RevenueReport";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
